b6dc0fc9f729ba8c14f4023e8e3a4ea76cb247de
ManuelOliverNacher <manueloliver6@gmail.com>
2022-05-31 22:55:36 -0300

    Entrega desafio

---
+++ Desafio Complementario 1-Manuel Oliver Nacher/js/main.js
@@ -0,0 +1,21 @@
+ let nombre= prompt("Ingrese su nombre y apellido. \n para salir ingrese SALIR");
+ let comision=prompt("Ingrese su comision");
+  
+ 
+ while(nombre != "SALIR"){
+
+    console.log("Tu nombre es: " + nombre + ", esta cursando JavaScript en CoderHouse y sos de parte de la comision:" + comision );
+    
+    nombre= prompt("Ingrese su nombre y apellido. \n para salir ingrese SALIR"); 
+     
+   }
+  
+   
+
+   
+   
+    
+  
+
+ 
+ 
